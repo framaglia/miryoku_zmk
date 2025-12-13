750d70c29358f51666c81319627380af3213f999
francesco.maglia <francesco.maglia@treatwell.com>
2025-12-13 12:46:21 +0000

    re-adding gaming layer

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,10 +1,24 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+// Define GAME layer key bindings FIRST (before layer list override)
+// This ensures MIRYOKU_ALTERNATIVES_GAME is defined when needed
+#define MIRYOKU_ALTERNATIVES_GAME \
+&kp R,             &kp E,             &kp G,             &kp T,             &kp TAB,           U_NP,              U_NP,             U_NP,             U_NP,             U_NP,             \
+&kp W,             &kp A,             &kp S,             &kp D,             &kp F,             U_NP,              U_NP,             U_NP,             U_NP,             U_NP,             \
+&kp Q,             &kp Z,             &kp X,             &kp C,             &kp V,             U_NP,              U_NP,             U_NP,             U_NP,             U_NP,             \
+U_NP,              U_NP,              &kp LCTRL,         &kp SPACE,         &kp LSHFT,         U_NP,              U_NP,             U_NP,             U_NP,              U_NP
+
+// Define GAME layer selection (before layer list override)
+#if !defined(MIRYOKU_LAYER_GAME)
+  #define MIRYOKU_LAYER_GAME MIRYOKU_ALTERNATIVES_GAME
+#endif
+#if !defined(MIRYOKU_LAYERMAPPING_GAME)
+  #define MIRYOKU_LAYERMAPPING_GAME MIRYOKU_MAPPING
+#endif
+
 // Add GAME layer to the layer list
-// Must be defined before miryoku.h is included
-// TEST: Temporarily disable override to see if base build works
-/*
+// Must be defined before miryoku.h is included (which includes miryoku_layer_list.h)
 #undef MIRYOKU_LAYER_LIST
 #define MIRYOKU_LAYER_LIST \
 MIRYOKU_X(BASE,   "Base") \
@@ -18,34 +32,11 @@ MIRYOKU_X(NUM,    "Num") \
 MIRYOKU_X(SYM,    "Sym") \
 MIRYOKU_X(FUN,    "Fun") \
 MIRYOKU_X(GAME,   "Game")
-*/
 
-// Define GAME layer number (must be defined after MIRYOKU_LAYER_LIST override)
-// This ensures U_GAME is available when needed
-//#ifndef U_GAME
-//#define U_GAME 10
-//#endif
-
-// Define GAME layer key bindings for Warzone (LEFT HAND ONLY)
-// Layout: Left half only - right side disabled for mouse use
-// Mouse handles: Aim, shoot, weapon switching (mouse wheel)
-// Top row: Reload (R), Interact (E), Ping (G), Chat (T), Scoreboard (TAB) | Right side disabled
-// Middle row: Movement (WASD), Interact (F) | Right side disabled  
-// Bottom row: Equipment (Q), Prone (Z), Equipment wheel (X), Melee (C), Armor plates (V) | Right side disabled
-// Thumbs: Ctrl (crouch), Space (jump), Shift (sprint) | Right thumbs disabled
-//#define MIRYOKU_ALTERNATIVES_GAME \
-//&kp R,             &kp E,             &kp G,             &kp T,             &kp TAB,           U_NP,              U_NP,             U_NP,             U_NP,             U_NP,             \
-//&kp W,             &kp A,             &kp S,             &kp D,             &kp F,             U_NP,              U_NP,             U_NP,             U_NP,             U_NP,             \
-//&kp Q,             &kp Z,             &kp X,             &kp C,             &kp V,             U_NP,              U_NP,             U_NP,             U_NP,             U_NP,             \
-//U_NP,              U_NP,              &kp LCTRL,         &kp SPACE,         &kp LSHFT,         U_NP,              U_NP,             U_NP,             U_NP,              U_NP
-
-// Define GAME layer selection
-//#if !defined(MIRYOKU_LAYER_GAME)
-//  #define MIRYOKU_LAYER_GAME MIRYOKU_ALTERNATIVES_GAME
-//#endif
-//#if !defined(MIRYOKU_LAYERMAPPING_GAME)
-//  #define MIRYOKU_LAYERMAPPING_GAME MIRYOKU_MAPPING
-//#endif
+// Define GAME layer number (after layer list, but before it's used)
+#ifndef U_GAME
+#define U_GAME 10
+#endif
 
 // GAME Layer Activation:
 // 
