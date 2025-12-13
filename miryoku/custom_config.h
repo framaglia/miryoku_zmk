2dca6302cdcc7798bcafd6273229419528194a38
francesco.maglia <francesco.maglia@treatwell.com>
2025-12-13 12:38:07 +0000

    try fix build

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -3,6 +3,8 @@
 
 // Add GAME layer to the layer list
 // Must be defined before miryoku.h is included
+// TEST: Temporarily disable override to see if base build works
+/*
 #undef MIRYOKU_LAYER_LIST
 #define MIRYOKU_LAYER_LIST \
 MIRYOKU_X(BASE,   "Base") \
@@ -15,13 +17,14 @@ MIRYOKU_X(MEDIA,  "Media") \
 MIRYOKU_X(NUM,    "Num") \
 MIRYOKU_X(SYM,    "Sym") \
 MIRYOKU_X(FUN,    "Fun") \
-//MIRYOKU_X(GAME,   "Game")
+MIRYOKU_X(GAME,   "Game")
+*/
 
 // Define GAME layer number (must be defined after MIRYOKU_LAYER_LIST override)
 // This ensures U_GAME is available when needed
-#ifndef U_GAME
-#define U_GAME 10
-#endif
+//#ifndef U_GAME
+//#define U_GAME 10
+//#endif
 
 // Define GAME layer key bindings for Warzone (LEFT HAND ONLY)
 // Layout: Left half only - right side disabled for mouse use
@@ -30,19 +33,19 @@ MIRYOKU_X(FUN,    "Fun") \
 // Middle row: Movement (WASD), Interact (F) | Right side disabled  
 // Bottom row: Equipment (Q), Prone (Z), Equipment wheel (X), Melee (C), Armor plates (V) | Right side disabled
 // Thumbs: Ctrl (crouch), Space (jump), Shift (sprint) | Right thumbs disabled
-#define MIRYOKU_ALTERNATIVES_GAME \
-&kp R,             &kp E,             &kp G,             &kp T,             &kp TAB,           U_NP,              U_NP,             U_NP,             U_NP,             U_NP,             \
-&kp W,             &kp A,             &kp S,             &kp D,             &kp F,             U_NP,              U_NP,             U_NP,             U_NP,             U_NP,             \
-&kp Q,             &kp Z,             &kp X,             &kp C,             &kp V,             U_NP,              U_NP,             U_NP,             U_NP,             U_NP,             \
-U_NP,              U_NP,              &kp LCTRL,         &kp SPACE,         &kp LSHFT,         U_NP,              U_NP,             U_NP,             U_NP,              U_NP
+//#define MIRYOKU_ALTERNATIVES_GAME \
+//&kp R,             &kp E,             &kp G,             &kp T,             &kp TAB,           U_NP,              U_NP,             U_NP,             U_NP,             U_NP,             \
+//&kp W,             &kp A,             &kp S,             &kp D,             &kp F,             U_NP,              U_NP,             U_NP,             U_NP,             U_NP,             \
+//&kp Q,             &kp Z,             &kp X,             &kp C,             &kp V,             U_NP,              U_NP,             U_NP,             U_NP,             U_NP,             \
+//U_NP,              U_NP,              &kp LCTRL,         &kp SPACE,         &kp LSHFT,         U_NP,              U_NP,             U_NP,             U_NP,              U_NP
 
 // Define GAME layer selection
-#if !defined(MIRYOKU_LAYER_GAME)
-  #define MIRYOKU_LAYER_GAME MIRYOKU_ALTERNATIVES_GAME
-#endif
-#if !defined(MIRYOKU_LAYERMAPPING_GAME)
-  #define MIRYOKU_LAYERMAPPING_GAME MIRYOKU_MAPPING
-#endif
+//#if !defined(MIRYOKU_LAYER_GAME)
+//  #define MIRYOKU_LAYER_GAME MIRYOKU_ALTERNATIVES_GAME
+//#endif
+//#if !defined(MIRYOKU_LAYERMAPPING_GAME)
+//  #define MIRYOKU_LAYERMAPPING_GAME MIRYOKU_MAPPING
+//#endif
 
 // GAME Layer Activation:
 // 
