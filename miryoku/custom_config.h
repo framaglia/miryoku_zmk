86fcaafd27fa812b6e56157b718752cc54d0bca2
francesco.maglia <francesco.maglia@treatwell.com>
2025-12-13 20:13:19 +0000

    try fix again

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -6,6 +6,19 @@
 
 #define XXX &none
 
+// Define layer numbers FIRST (before MIRYOKU_LAYER_LIST so they're available when needed)
+#define U_BASE        0
+#define U_EXTRA       1
+#define U_TAP         2
+#define U_BUTTON      3
+#define U_NAV         4
+#define U_MOUSE       5
+#define U_MEDIA       6
+#define U_NUM         7
+#define U_SYM         8
+#define U_FUN         9
+#define U_GAME       10
+
 // Warzone gaming layer (LEFT HAND ONLY - right side disabled for mouse use)
 // Mouse handles: Aim, shoot, weapon switching (mouse wheel)
 // QWERTY layout with gaming bindings:
@@ -44,15 +57,3 @@ XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
 XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
 XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
                K32  K33  K34       K35  K36  K37
-
-#define U_BASE        0
-#define U_EXTRA       1
-#define U_TAP         2
-#define U_BUTTON      3
-#define U_NAV         4
-#define U_MOUSE       5
-#define U_MEDIA       6
-#define U_NUM         7
-#define U_SYM         8
-#define U_FUN         9
-#define U_GAME       10
