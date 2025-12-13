900a2f836f1dae616179892ee379f7603353f9a0
francesco.maglia <francesco.maglia@treatwell.com>
2025-12-13 20:30:03 +0000

    try adding one piece

---
+++ miryoku/custom_config.h
@@ -0,0 +1,19 @@
+// Copyright 2021 Manna Harbour
+// https://github.com/manna-harbour/miryoku
+
+//#define MIRYOKU_KLUDGE_DOUBLETAPBOOT
+#define MIRYOKU_KLUDGE_SOFT_OFF
+
+// Step 1: Copy ALL layer number definitions from miryoku_layer_list.h
+#define U_BASE   0
+#define U_EXTRA  1
+#define U_TAP    2
+#define U_BUTTON 3
+#define U_NAV    4
+#define U_MOUSE  5
+#define U_MEDIA  6
+#define U_NUM    7
+#define U_SYM    8
+#define U_FUN    9
+#define U_GAME  10
+
